3d0aa9acba2118d6343b557552d225d88d768ac8
vvd170501 <36827317+vvd170501@users.noreply.github.com>
2022-12-16 21:20:24 +0300

    Add magic numbers check from clang-tidy-15

--- prepare.sh
+++ prepare.sh
@@ -8,8 +8,8 @@ wget "https://github.com/llvm/llvm-project/releases/download/llvmorg-${LLVM_VERS
 tar xf clang-tools-extra.tar.xz
 rm clang-tools-extra.tar.xz
 
-for file in $(ls clang-tidy); do
-    cp clang-tools-extra*/clang-tidy/$file clang-tidy/
+for file in $(find clang-tidy -name '*.h'); do
+    cp clang-tools-extra*/$file $file
 done
 
 rm -r clang-tools-extra*

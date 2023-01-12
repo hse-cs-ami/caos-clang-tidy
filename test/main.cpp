47a303752f5ff68e3189e9d9b494a4fc8f85297a
vvd170501 <36827317+vvd170501@users.noreply.github.com>
2023-01-13 01:30:17 +0300

    Update test files

--- test/main.cpp
+++ test/main.cpp
@@ -1,7 +1,5 @@
-long strtol(const char *nptr, char **endptr, int base);
-
-const int foo = 123;  // should trigger a warning ("const" is not a compile-time constant)
-const float foo2 = 123.4;  // should trigger a warning ("const" is not a compile-time constant
+const int foo = 123;  // should not trigger any warnings. "const" is a compile-time constant.
+const float foo2 = 123.4;
 
 struct bad_struct {  // should trigger a warning (bad case)
     int x;
@@ -26,9 +24,8 @@ int main() {
     int qwe = bar + 321;  // should trigger a warning (magic number)
 
     // should not trigger any warnings
-    int xyz = strtol("123", 0, 10);  // integer literals are allowed in 3rd arg of strtol
     const struct GoodStruct ST = { .x = 123, .y = 456 };  // literals are allowed in initializer lists
     const union GoodUnion UN = { .i = 123 };
-    const int SOME_INTS[] = { 123, 456, [10] = 999 };
+    const int SOME_INTS[] = { 123, 456 };
     const int SOME_MORE_INTS[][2] = {{ 123, 456 }, { 789, 987 }, { 111, 222 } };
 }
